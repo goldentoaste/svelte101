cd27665c9b017cbc4bdb7f0e5d5694f7925fa23b
goldentoaste <rayg2375@gmail.com>
2024-05-15 03:10:52 -0700

    starting on demo project

---
+++ demo_app/src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { onNavigate } from "$app/navigation";
+    import NavBar, { type NavItem } from "$lib/NavBar.svelte";
+    
+    import "../global.css"; // its nice to have a style sheet applied to the entire app, this is one way to do it.
+
+    let navTargets: NavItem[] = [
+        {
+            name: "Home",
+            target: "/",
+        },
+        {
+            name: "Todo",
+            target: "/todo",
+        },
+        {
+            name: "About",
+            target: "/about",
+        },
+    ];
+
+
+</script>
+
+<NavBar navItems={navTargets} />
+
+<slot />
