c109530cb09059b49c846c7f449c1d3826a448d7
goldentoaste <rayg2375@gmail.com>
2025-09-30 21:05:14 -0700

    Update +page.svelte

--- main_page/src/routes/about/+page.svelte
+++ main_page/src/routes/about/+page.svelte
@@ -1,3 +1,8 @@
+<p>Svelte 101, a Svelte introduction slideshow site.</p>
 
-Svelte 101, a Svelte introduction slideshow site.
-Last updated for Stormhacks 2025 Workshop, Oct, 1st. Using Svelte V5.38.0
+<p>Last updated for Stormhacks 2025 Workshop, Oct, 1st. Using Svelte V5.38.0</p>
+
+<p>
+    <a href="https://github.com/goldentoaste/svelte101">https://github.com/goldentoaste/svelte101</a
+    >
+</p>
